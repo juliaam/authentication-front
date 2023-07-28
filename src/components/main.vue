7a16d770ff1a2e8cb06e80558d95ca3c90906d90
Julia Moraes <juliademoraess@gmail.com>
2023-07-27 21:55:26 -0300

    feat: box com css bem gatinho

---
+++ src/components/main.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+	<div class="container">
+		<div class="container-in">
+			<div class="box">
+				Criar conta
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.container {
+	width: 900px;
+	height: 500px;
+	background-color: transparent;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-style: solid;	
+	border-width: 2px;
+	border-color: rgb(239, 238, 238);
+	border-radius: 30px;
+}
+.container-in {
+	width: 70%;
+	height: 100%;
+	display: flex;
+	margin-left: auto;
+	align-items: right;
+	justify-content: right;
+	background-color: rgb(243, 235, 235);
+	border-radius: 25px;
+	border-color: rgb(247, 241, 241);
+	border-width: 2px;
+	border-style: solid;
+}
+.box {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: black;
+	margin: auto;
+}
+p {
+	color: black;
+}
+</style>
