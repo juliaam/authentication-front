4fc28f037da0fe4b90dc56ea5a0eae078589d4df
júlia moraes <juliademoraess@gmail.com>
2024-06-05 17:23:21 -0300

    fix: user service

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { User } from "../types/User";
 
 export class UserService {
-  private static apiUrl = "https://localhost:3000/users";
+  private static apiUrl = "https://localhost:3000/api/users";
 
   static register(body: User) {
-    return axios.post(this.apiUrl, body);
+    return axios.post(`${this.apiUrl}/register`, body);
   }
 }
