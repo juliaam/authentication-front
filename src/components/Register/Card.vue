7c80a5e624d046d929e874ea5b2dc4776467507f
júlia moraes <juliademoraess@gmail.com>
2024-06-09 14:23:14 -0300

    fix: componentization and colors archive

---
+++ src/components/Register/Card.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { StyleValue } from "vue";
+
+defineProps<{
+  src: string;
+  alt: string;
+  text: string;
+  style: StyleValue;
+}>();
+</script>
+
+<template>
+  <div class="card" :style="style">
+    <img class="card__img" :src="src" :alt="alt" />
+    <div class="card__text">
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use "/src/styles/colors.scss" as colors;
+@use "/src/styles/fonts.scss" as fonts;
+
+.card {
+  max-height: 5rem;
+
+  font-size: 1.2rem;
+  font-weight: fonts.$bold;
+  color: colors.$gray;
+
+  display: flex;
+
+  gap: 12rem;
+
+  padding-top: 0.1rem;
+  padding-bottom: 0.1rem;
+  padding-inline: 1rem;
+  align-items: center;
+
+  border: 1px solid colors.$gray;
+  border-radius: 0.7rem;
+
+  cursor: pointer;
+
+  &__img {
+    max-width: 35px;
+    max-height: 35px;
+  }
+}
+</style>
--- src/components/Register/Register.vue
+++ src/components/Register/Register.vue
@@ -1,26 +1,14 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import facebook from "./img/facebookIcon.svg";
 import google from "./img/googleIcon.svg";
 import loginIllustration from "./img/loginIllustration.svg";
 import icon from "./img/keyIcon.svg";
+import Card from "./Card.vue";
+import Input from "../Input.vue";
 
-const $nameLabel = ref(null);
-const $emailLabel = ref(null);
-const $passwordLabel = ref(null);
-
-const formRegister = ref({
-  name: { value: "", isFocusing: false },
-  email: { value: "", isFocusing: false },
-  password: { value: "", isFocusing: false },
-});
-
-const activeLabel = (
-  elementRef: any,
-  value: { value: string; isFocusing: boolean }
-) => {
-  elementRef.classList.add("label--active");
-  value.isFocusing = true;
+const form = {
+  name: "",
+  email: "",
+  password: "",
 };
 </script>
 
@@ -38,55 +26,32 @@ const activeLabel = (
       <div class="container-register__in--content">
         <h2 class="container-register__title">Criar conta</h2>
         <div class="container-register__card-group">
-          <div class="container-register__card">
-            <img
-              class="container-register__img"
-              :src="google"
-              alt="google icon"
-            />
-            <p class="container-register__card--text">Acessar com google</p>
-          </div>
-          <div class="container-register__card">
-            <img
-              class="container-register__img"
-              :src="facebook"
-              alt="facebook icon"
-            />
-            <p class="container-register__card--text">Acessar com Facebook</p>
-          </div>
+          <Card
+            :style="{ width: '100%' }"
+            :src="google"
+            alt="Google Icon"
+            text="Registrar com google"
+          />
         </div>
         <div class="container-register__input-group">
-          <label class="container-register__label" for="name" ref="$nameLabel"
-            >Nome Completo
-          </label>
-          <input
-            @focus="activeLabel($nameLabel, formRegister.name)"
-            v-model="formRegister.name.value"
-            class="container-register__input"
-            :placeholder="formRegister.name.isFocusing ? '' : 'Nome completo'"
+          <Input
+            label="Nome"
+            v-model="form.name"
+            placeholder="Nome"
+            style="margin-top: 1rem"
           />
-          <label class="container-register__label" for="email" ref="$emailLabel"
-            >E-mail</label
-          >
-          <input
-            @focus="activeLabel($emailLabel, formRegister.email)"
-            v-model="formRegister.email.value"
-            name="email"
-            class="container-register__input"
-            :placeholder="formRegister.email.isFocusing ? '' : 'E-mail'"
+          <Input
+            label="Email"
+            v-model="form.email"
+            placeholder="Email"
+            style="margin-top: 1rem"
           />
-          <label
-            class="container-register__label"
-            for="password"
-            ref="$passwordLabel"
-            >Senha</label
-          >
-          <input
-            @focus="activeLabel($passwordLabel, formRegister.password)"
-            v-model="formRegister.password.value"
-            name="password"
-            class="container-register__input"
-            :placeholder="formRegister.password.isFocusing ? '' : 'Senha'"
+
+          <Input
+            label="Senha"
+            v-model="form.password"
+            placeholder="Senha"
+            style="margin-top: 1rem"
           />
         </div>
         <button class="container-register__button">Criar conta</button>
@@ -140,29 +105,14 @@ const activeLabel = (
 
   &__card-group {
     display: flex;
-    margin-bottom: 5rem;
-    justify-content: space-between;
-  }
 
-  &__card {
-    justify-content: space-evenly;
-    display: flex;
-    align-items: center;
-    border: 1px solid colors.$lightGray;
-    border-radius: 0.7rem;
-    width: 48%;
-    padding-top: 0.3rem;
-    padding-bottom: 0.3rem;
-    cursor: pointer;
-    font-size: 1.2rem;
-    font-weight: fonts.$bold;
-    color: colors.$gray;
+    justify-content: end;
   }
 
   &__button {
     min-width: 100%;
     height: 4rem;
-    background-color: rgba(74, 18, 161, 255);
+    background-color: colors.$primary;
     color: whitesmoke;
     border: none;
     border-radius: 0.5rem;
@@ -173,11 +123,6 @@ const activeLabel = (
     font-weight: fonts.$semiBold;
     font-family: fonts.$mainFont;
     cursor: pointer;
-  }
-
-  &__img {
-    width: 35px;
-    height: 35px;
   }
 
   &__label {
@@ -196,32 +141,21 @@ const activeLabel = (
     transform: translateY(0);
   }
 
-  &__input {
-    min-width: 100%;
-    background-color: transparent;
-    border: none;
-    border-bottom: 0.05rem solid colors.$lightGray;
-    box-sizing: border-box;
-    margin-bottom: 1.4rem;
-
-    font-weight: fonts.$normal;
-    font-family: fonts.$mainFont;
-    font-size: 1.7rem;
-    color: colors.$gray;
-  }
-  &__input:focus {
-    outline: none;
-  }
-
   &__bottom {
     display: flex;
   }
   &__text {
     color: colors.$gray;
+
+    display: flex;
+    justify-content: center;
+
     &--clickable {
-      color: rgba(74, 18, 161, 255);
+      color: colors.$black;
+      font-weight: fonts.$semiBold;
+
       margin-left: 0.7rem;
-      font-weight: fonts.$thin;
+
       cursor: pointer;
     }
   }
@@ -231,13 +165,16 @@ const activeLabel = (
   .container-register {
     &__in {
       &--content {
-        margin: 4rem 5rem auto 5rem;
+        display: flex;
+
+        gap: 0.6rem;
+
+        padding: 4rem;
       }
     }
     &__button {
       margin-top: 45%;
     }
-    
   }
 }
 
@@ -259,19 +196,24 @@ const activeLabel = (
     &__out {
       min-width: 40%;
       height: 100%;
+
       display: block !important;
     }
+
     &--content {
       display: flex;
       flex-direction: column;
       color: colors.$lightGray;
       margin: 8rem 10rem auto 10rem;
     }
+
     &__in {
       height: 100%;
+      width: 60%;
     }
+
     &__button {
-      margin-top: 3rem;
+      margin-top: 1rem;
     }
   }
 }
