accfdef4644fddee9dd34c96d0313d306139f5b5
júlia moraes <juliademoraess@gmail.com>
2024-06-09 15:17:11 -0300

    feat: pinia in root

--- src/services/user.ts
+++ src/services/user.ts
@@ -4,7 +4,7 @@ import { User } from "../types/User";
 export class UserService {
   private static apiUrl = "https://localhost:3000/api/users";
 
-  static register(body: User) {
-    return axios.post(`${this.apiUrl}/register`, body);
+  static async register(body: User) {
+    return await axios.post(`${this.apiUrl}/register`, body);
   }
 }
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,5 +1,5 @@
-export type User = {
+export interface User {
   name: string;
   email: string;
   password: string;
-};
+}
